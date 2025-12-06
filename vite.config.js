39166dea3b09344343a26ae453a04b0038677a6b
Mohamed <mohameddarhoub36@gmail.com>
2025-12-06 15:12:12 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/m7sd-Portofolio_V5/',   // 👈 MUST match your GitHub repo name exactly
 })
